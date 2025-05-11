d72bd3e93e282aa6138f1a8b968e53187217b4e8
Wolfinhoc <broccowolfardt@gmail.com>
2025-05-11 15:47:03 -0300

    git commit teste

--- aulas/aula14042025_poo/GerenciadorDeCompromissos/GerenciadorDeCompromissos/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ aulas/aula14042025_poo/GerenciadorDeCompromissos/GerenciadorDeCompromissos/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+533113d0d5a1073314768c4d4231e76f1d267168")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0da8497b1bae878eed7b82fe708f37f838e6cbeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
